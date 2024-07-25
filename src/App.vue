ecacc1595b4a7809a083b74afc678ec534b2348a
qudcks8084 <qudcks8084@naver.com>
2024-07-25 19:00:24 +0900

    [ADD] Vue 기초 설정

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(eachmenu,index) in menubar" :key="index">{{eachmenu}} </a>
+  </div>
+
+  <div v-for="(eachData, index) in product" :key="index">
+    <h4>{{eachData.name}}</h4> 
+    <p>{{eachData.price}} 만원</p>
+    <button v-on:click="increase(eachData)">허위매물신고</button> <span>신고수 : {{ eachData.click }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){ // 데이터 보관
+    return{
+      menubar : ['Home', 'Product', 'About'],
+      product : [
+        {name : '역삼동원룸', price : 10, click : 0},
+        {name : '천호동원룸', price : 20, click : 0},
+        {name : '마포구원룸', price : 30, click : 0}
+      ],
+    }
+  },
+
+  methods : {
+    increase(eachData){
+      eachData.click +=1 ;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +42,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding : 15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>
